b23cf1259ac814863fb4b60feaf7d468282a9e64
Jack Kemp <72166412+jackwkemp@users.noreply.github.com>
2021-09-21 16:07:47 +0200

    fixed layout, redid css, added video place holder

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 // Opens and closes the demo gif for Project 1
 
 // Get the modal
-var modal = document.getElementById("myModal1");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImgP1");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closeP1")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
+// var modal = document.getElementById("myModal1");
+//
+// // Get the image and insert it inside the modal - use its "alt" text as a caption
+// var img = document.getElementById("myImgP1");
+// var modalImg = document.getElementById("img01");
+// var captionText = document.getElementById("caption");
+// img.onclick = function () {
+//     modal.style.display = "block";
+//     modalImg.src = this.src;
+//     captionText.innerHTML = this.alt;
+// }
+//
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("closeP1")[0];
+//
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function () {
+//     modal.style.display = "none";
+// }
 ////////////////////////////////////////////////////////
 
 // Opens and closes the demo gif for Project 2
